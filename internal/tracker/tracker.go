db6348e577a07f74a56f7aedc9dc2a63268c9ba9
Martin Gallauner <martin.gallauner@pm.me>
2024-08-25 11:35:20 +0200

    adds mock repository

--- internal/tracker/tracker.go
+++ internal/tracker/tracker.go
@@ -1,8 +1,17 @@
 package tracker
 
 type tracker struct {
+	repository Repository
 }
 
-func GetLevel() int {
-	return 0
+func New(repository Repository) *tracker {
+	return &tracker{repository: repository}
+}
+
+type Repository interface {
+	fetch() int
+}
+
+func (tracker *tracker) GetLevel() int {
+	return tracker.repository.fetch()
 }
--- internal/tracker/tracker_test.go
+++ internal/tracker/tracker_test.go
@@ -2,8 +2,16 @@ package tracker
 
 import "testing"
 
+type TestRepository struct {
+}
+
+func (r *TestRepository) fetch() int {
+	return 0
+}
+
 func TestLevelIsZero(t *testing.T) {
-	caffeineLevel := GetLevel()
+	tracker := New(&TestRepository{})
+	caffeineLevel := tracker.GetLevel()
 
 	if caffeineLevel != 0 {
 		t.Errorf("Expected zero but got '%v'", caffeineLevel)
