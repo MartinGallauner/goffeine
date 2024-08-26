3ce12c52bdf1541b46150f3fba710eae11eca42d
Martin Gallauner <martin.gallauner@pm.me>
2024-08-26 10:31:13 +0200

    adds parsing of entries

--- internal/tracker/tracker.go
+++ internal/tracker/tracker.go
@@ -1,6 +1,11 @@
 package tracker
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+)
 
 type Tracker struct {
 	repository Repository
@@ -15,12 +20,44 @@ type Repository interface {
 	Add(caffeineInMg int) error
 }
 
-func (tracker *Tracker) GetLevel() {
+type Entry struct {
+	timestamp    time.Time
+	caffeineInMg int
+}
+
+func (tracker *Tracker) GetLevel() (int, error) {
 	//todo calculate level for right now
 	//todo cleanup entries older than 24h
 	data, _ := tracker.repository.Fetch()
-	log.Println(data)
 
+	//parse data
+	data = data[1:] //ignore the header row
+	layout := "2006-01-02T15:04:05Z07:00"
+	caffeineLevel := 0
+	for _, row := range data {
+		timestamp, err := time.Parse(layout, row[0])
+		if err != nil {
+			return 0, err
+		}
+
+		value := parseInt(row[1])
+
+		if time.Now().Add(-1 * 24 * time.Hour).Before(timestamp) {
+			caffeineLevel += value
+		}
+	}
+	log.Printf("You have %q of caffeine in your system")
+	return caffeineLevel, nil //todo calculate level
+}
+
+func parseInt(s string) int {
+	// Implement error handling and conversion logic as needed
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println("Error converting to int:", err)
+		return 0 // Or handle the error differently
+	}
+	return i
 }
 
 func (tracker *Tracker) Add(caffeineInMg int) {
