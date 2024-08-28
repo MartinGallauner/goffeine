755a614bea5a361945b29810f2f2515a6ffc886b
Martin Gallauner <martin.gallauner@pm.me>
2024-08-28 08:25:30 +0200

    cleanup

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/MartinGallauner/goffeine/internal/repl"
 	"github.com/MartinGallauner/goffeine/internal/repository"
 	"github.com/MartinGallauner/goffeine/internal/tracker"
 	"log"
-	"os"
-	"strconv"
-	"time"
 )
 
 func main() {
@@ -18,42 +14,4 @@ func main() {
 	t := tracker.New(csvRepository)
 	config := &repl.Config{Tracker: *t}
 	repl.StartRepl(config)
-
-	command, num, err := processArgs(os.Args[1:])
-	if err != nil {
-		log.Fatalf("Unable to handle passed arguments. Got %q", os.Args)
-	}
-
-	switch command {
-	case "add":
-		t.Add(time.Now(), num)
-	case "status":
-		t.GetLevel(time.Now())
-	}
-	log.Println("Finished Goffeine")
-}
-
-func processArgs(args []string) (string, int, error) {
-	if len(args) < 1 {
-		log.Fatal("Please provide an argument")
-	}
-
-	command := args[0]
-
-	if command == "add" {
-
-		if len(args) < 2 {
-			log.Fatal("Please provide a number as 2nd argument")
-		}
-
-		num, err := strconv.Atoi(args[1])
-		if err != nil {
-			// handle error
-			fmt.Println("Error:", err)
-			return "", 0, nil
-		}
-		return command, num, nil
-	}
-	return command, 0, nil
-
 }
