5e5b12cbd30374110a0cb8c9a5b3c7d98e192fca
MartinGallauner <martin.gallauner@pm.me>
2024-10-15 15:59:01 +0200

    introduces templ and returns very simple html handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"time"
+	"github.com/a-h/templ"
 )
 
 type GoffeineServer struct {
@@ -20,7 +21,10 @@ func NewGoffeineServer(tracker Tracker) *GoffeineServer {
 	router := http.NewServeMux()
 	router.Handle("/api/status", http.HandlerFunc(s.statusHandler))
 	router.Handle("/api/add", http.HandlerFunc(s.intakeHandler))
-	router.Handle("/", http.HandlerFunc(s.htmlHandler))
+	//router.Handle("/", http.HandlerFunc(s.htmlHandler))
+
+	component := hello("galle")
+	router.Handle("/", templ.Handler(component))
 
 	s.Handler = router
 
@@ -34,42 +38,29 @@ type Tracker interface {
 
 func (s *GoffeineServer) htmlHandler(w http.ResponseWriter, r *http.Request) {
 
-	html := `
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Simple Go HTML Server</title>
-	</head>
-	<body>
-		<h1>Hello from Go!</h1>
-		<p>This is a simple HTML page served by a Go application.</p>
-	</body>
-	</html>
-	`
-	
+	component := hello("galle")
+
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, html)
+	fmt.Fprint(w, component)
 
 }
 
 func (s *GoffeineServer) statusHandler(w http.ResponseWriter, r *http.Request) {
 	level, _ := s.Tracker.GetLevel(time.Now()) // TODO handle error
-		fmt.Fprint(w, level)
-		return
+	fmt.Fprint(w, level)
+	return
 }
 
 func (s *GoffeineServer) intakeHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Unable to read request body", http.StatusBadRequest)
-			return
-		}
-		defer r.Body.Close()
-		w.WriteHeader(http.StatusAccepted)
-
-		s.Tracker.Add(string(body))
-		fmt.Fprint(w, nil)
+	if err != nil {
+		http.Error(w, "Unable to read request body", http.StatusBadRequest)
 		return
+	}
+	defer r.Body.Close()
+	w.WriteHeader(http.StatusAccepted)
+
+	s.Tracker.Add(string(body))
+	fmt.Fprint(w, nil)
+	return
 }
