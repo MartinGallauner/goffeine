9036e7f5830823bc0710f077bc61b5053f1d67ef
Martin Gallauner <martin.gallauner@pm.me>
2024-11-25 16:31:33 +0100

    reformat

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"time"
 	"github.com/MartinGallauner/goffeine/internal/askopenai"
 	"github.com/MartinGallauner/goffeine/internal/server"
 	"github.com/MartinGallauner/goffeine/internal/tracker"
 	"github.com/alexedwards/scs/v2"
 	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"time"
 )
-
 
 func main() {
 	log.Println("Started Goffeine")
@@ -22,13 +21,12 @@ func main() {
 
 	repository := tracker.NewMemoryRepository()
 	client := askopenai.New()
-	
+
 	t := tracker.New(repository, client)
-	
-	
+
 	sessionManager := scs.New()
 	sessionManager.Lifetime = 24 * time.Hour
 	server := server.NewGoffeineServer(t, sessionManager)
-	
-	log.Fatal(http.ListenAndServe(":5001", server))
+
+	log.Fatal(http.ListenAndServe(":8080", server))
 }
