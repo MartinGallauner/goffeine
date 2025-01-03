f2c9adb525d3975b0d8d5f35d086e80d911829b1
Martin Gallauner <martin.gallauner@pm.me>
2025-01-03 12:03:52 +0100

    Revert "Resolves finding of staticcheck"
    This reverts commit 2d0c2a2162c33bf06e868ffab70ce60ca64ea460.

--- internal/server/server.go
+++ internal/server/server.go
@@ -64,6 +64,7 @@ func (s *GoffeineServer) handlePagePost(w http.ResponseWriter, r *http.Request) 
 func (s *GoffeineServer) statusHandler(w http.ResponseWriter, r *http.Request) {
 	level, _ := s.Tracker.GetLevel(time.Now()) // TODO handle error
 	fmt.Fprint(w, level)
+	return
 }
 
 func (s *GoffeineServer) intakeHandler(w http.ResponseWriter, r *http.Request) {
@@ -77,4 +78,5 @@ func (s *GoffeineServer) intakeHandler(w http.ResponseWriter, r *http.Request) {
 
 	s.Tracker.Add(string(body))
 	fmt.Fprint(w, nil)
+	return
 }
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"github.com/MartinGallauner/goffeine/internal/tracker"
 	"net/http"
 	"net/http/httptest"
@@ -26,7 +27,7 @@ func TestGETStatusUser(t *testing.T) {
 }
 
 func newGetStatusRequest(userId string) *http.Request {
-	req, _ := http.NewRequest(http.MethodGet, "/api/status", nil)
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/api/status"), nil)
 	return req
 }
 
